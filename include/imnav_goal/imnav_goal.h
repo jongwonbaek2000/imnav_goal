cf65bee919887f1a77766dcb84f92dd77f5eef3c
jongwonbaek2000 <jongwonbaek2000@gmail.com>
2024-07-14 00:02:42 +0900

    oneshot topic advertising for same goal

--- include/imnav_goal/imnav_goal.h
+++ include/imnav_goal/imnav_goal.h
@@ -5,6 +5,7 @@
 #include <sensor_msgs/NavSatFix.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <GeographicLib/UTMUPS.hpp>
+#include <cmath>
 
 class ImnavGoal {
 public:
@@ -19,12 +20,13 @@ private:
     sensor_msgs::NavSatFix origin_gps_;
     bool origin_received_;
     sensor_msgs::NavSatFix last_goal_;
-    bool new_goal_received_;
+    bool last_goal_published_;
 
     void originCallback(const sensor_msgs::NavSatFix::ConstPtr& msg);
     void goalCallback(const sensor_msgs::NavSatFix::ConstPtr& msg);
     std::tuple<double, double, int> gpsToUTM(double lat, double lon);
     void processAndPublishGoal();
+    bool isNewGoal(const sensor_msgs::NavSatFix& new_goal);
 };
 
 #endif // IMNAV_GOAL_H
--- src/imnav_goal_node.cpp
+++ src/imnav_goal_node.cpp
@@ -1,6 +1,6 @@
 #include <imnav_goal/imnav_goal.h>
 
-ImnavGoal::ImnavGoal() : origin_received_(false), new_goal_received_(false) {
+ImnavGoal::ImnavGoal() : origin_received_(false), last_goal_published_(false) {
     origin_sub_ = nh_.subscribe("/origin_gps", 1, &ImnavGoal::originCallback, this);
     goal_sub_ = nh_.subscribe("/gps_goal_fix", 1, &ImnavGoal::goalCallback, this);
     goal_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1);
@@ -20,14 +20,21 @@ void ImnavGoal::goalCallback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
         return;
     }
 
-    last_goal_ = *msg;
-    new_goal_received_ = true;
-    ROS_INFO("Goal GPS received: Lat=%f, Lon=%f", last_goal_.latitude, last_goal_.longitude);
-    processAndPublishGoal();
+    if (isNewGoal(*msg)) {
+        last_goal_ = *msg;
+        last_goal_published_ = false;
+        processAndPublishGoal();
+    }
+}
+
+bool ImnavGoal::isNewGoal(const sensor_msgs::NavSatFix& new_goal) {
+    const double epsilon = 1e-7;  // Small threshold for floating point comparison
+    return std::abs(new_goal.latitude - last_goal_.latitude) > epsilon ||
+           std::abs(new_goal.longitude - last_goal_.longitude) > epsilon;
 }
 
 void ImnavGoal::processAndPublishGoal() {
-    if (!new_goal_received_) {
+    if (last_goal_published_) {
         return;
     }
 
@@ -37,11 +44,10 @@ void ImnavGoal::processAndPublishGoal() {
     if (origin_zone != goal_zone) {
         ROS_WARN("Origin and goal are in different UTM zones. This may cause inaccuracies.");
     }
-    ROS_INFO("Origin UTM: x=%f, y=%f", origin_x, origin_y);
-    ROS_INFO("Goal UTM: x=%f, y=%f", goal_x, goal_y);
+
     double x_star = goal_x - origin_x;
     double y_star = goal_y - origin_y;
-    
+
     geometry_msgs::PoseStamped goal_msg;
     goal_msg.header.stamp = ros::Time::now();
     goal_msg.header.frame_id = "map";
@@ -52,7 +58,7 @@ void ImnavGoal::processAndPublishGoal() {
     goal_pub_.publish(goal_msg);
     ROS_INFO("Published new goal: x=%f, y=%f", x_star, y_star);
 
-    new_goal_received_ = false;
+    last_goal_published_ = true;
 }
 
 std::tuple<double, double, int> ImnavGoal::gpsToUTM(double lat, double lon) {
